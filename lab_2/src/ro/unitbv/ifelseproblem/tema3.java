72536e5a27050cb1aa1fa99e8b88623ffa758fd9
DanteHTB <43470248+DanteHTB@users.noreply.github.com>
2020-03-31 08:53:05 +0300

    laboratoruI 2

--- lab_2/src/ro/unitbv/ifelseproblem/tema3.java
+++ lab_2/src/ro/unitbv/ifelseproblem/tema3.java
@@ -4,10 +4,10 @@ public class tema3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Adress address=new Adress("Brasov ", "Nicopole ", 56);
+		Adress address=new Adress("Ploiesti ", "Vest ", 12);
 		PaymentMethod pm = new PaymentMethod("card");
 		Customer c=new Customer(address, pm);
-		Customer c1=new Customer("Pandrea", "Vlad ", 20);
+		Customer c1=new Customer("Angelescu", "Alexandru ", 20);
 		System.out.println(c1.getCompleteName());
 		System.out.println(c.getAdress().getCompleteAdress());
 		System.out.println(c.getPaymentMethod().getType());
